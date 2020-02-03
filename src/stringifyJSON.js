3149462d20151a527ded592ca8bdaeff9d1d5027
fasterthansky <fasterthansky@gmail.com>
2020-02-03 15:34:27 -0800

    working on stringify, add conditionl tocomplete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 
+// test for each type of data and convert to a string
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let result = [];
+  // test for an array first
+  if (Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+  }
+  // test for object
+  if (typeof obj === 'object' && obj) {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    for (var key in obj) {
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      // var value =  obj[key].toString();
+      // var keyName = key.toString();
+      // // let objName = keyName + ': ' + value;
+    }
+    return '{' + result + '}';
+  }
+  // test for a string
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // test for a number
+  // test for number boolean, null, undefined
+
+  //
+  return '' + obj;
+
 };
