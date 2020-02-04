678a589470731800dfe0fc83c73b417776e15c66
fasterthansky <fasterthansky@gmail.com>
2020-02-04 07:31:29 -0800

    Finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,26 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  //   results array for all elements with matching class
+  var result = [];
+//   function(document.body)
+
+  var targetClassName = function(node){
+//   if node class contains classname
+    if (node.classList) {
+     if (node.classList.contains(className)){
+//   push body into results
+      result.push(node);
+      }
+    }
+//   if childnodes contains class className
+    for(var i = 0; i < node.childNodes.length; i++){
+//   push child into results
+       targetClassName(node.childNodes[i]);
+    }
+  }
+//   jumpstart the function
+  targetClassName(document.body);
+//   return results
+  return result;
 };
